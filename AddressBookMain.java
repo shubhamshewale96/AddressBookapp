a7217cc95eeecf9ed1634f6fcf739d280e7d6e8c
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-10-07 19:51:06 +0530

    welcome to adress book

---
+++ AddressBookMain.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * @author shewale
+ *
+ */
+package com.bl.addressbook;
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("****\n Welcome To Address Book \n****\n");
+    }
+}
